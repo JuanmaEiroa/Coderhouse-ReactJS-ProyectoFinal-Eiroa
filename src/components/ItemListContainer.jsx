8fbfe5527eab4f568724fe1f5d2d1dec8bbe88d3
JuanmaEiroa <eiroajm@gmail.com>
2023-03-20 16:28:39 -0300

    Entrega Final: Agregado de favicon y de spinner loader

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import { useEffect, useState } from "react";
 import { collection, getDocs, getFirestore } from "firebase/firestore";
 import ItemList from "./ItemList";
-import { Container, Grid, Heading } from "@chakra-ui/react";
+import { Center, Container, Grid, Heading, Spinner } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
 
 const ItemListContainer = () => {
   const { category } = useParams();
   const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const db = getFirestore();
@@ -18,6 +19,7 @@ const ItemListContainer = () => {
       const docs = snapshot.docs.map((doc) => {
         return { id: doc.id, ...doc.data() };
       });
+      setIsLoading(false);
       category
         ? setProducts(docs.filter((product) => product.category === category))
         : setProducts(docs);
@@ -29,6 +31,11 @@ const ItemListContainer = () => {
       <Container className="productsTitle" centerContent>
         <Heading>Nuestros Productos</Heading>
       </Container>
+      {isLoading && (
+        <Center>
+          <Spinner thickness="4px" speed="0.50s" size="xl"/>
+        </Center>
+      )}
       <div className="productListContainer">
         <Grid templateColumns="repeat(3, 1fr)" gap={6}>
           <ItemList products={products} />
